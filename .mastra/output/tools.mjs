841227b250f6fd6c63cb60efa9ea1289a8af6ac9
trieu-1802 <trieuvuhai1802@gmail.com>
2026-01-20 11:24:42 +0700

    Remove .mastra

--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/ce3400b9-6000-48d8-b11f-bd8f7d81fbc4.mjs';
-import * as tool1 from './tools/bfe4d3aa-2dd5-41c6-bb00-1ca3550ef6d3.mjs';
-        
-                export const tools = [tool0, tool1]
